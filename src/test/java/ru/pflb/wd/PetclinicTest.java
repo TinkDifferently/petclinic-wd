e0ea2d617926bcf5f9b2c733f7563cc84ffed3c3
TinkDifferently <TinkDiferently@gmail.com>
2017-08-31 18:37:37 +0300

    finalize

--- src/test/java/ru/pflb/wd/PetclinicTest.java
+++ src/test/java/ru/pflb/wd/PetclinicTest.java
@@ -1,5 +1,6 @@
 package ru.pflb.wd;
 
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -12,7 +13,10 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.sql.Date;
 import java.util.concurrent.TimeUnit;
+//import static org.assertj.core.api.Assertions.assertThat;
+
 
 import static org.openqa.selenium.firefox.FirefoxDriver.PROFILE;
 
@@ -35,7 +39,50 @@ public class PetclinicTest {
 
         driver.findElement(By.xpath("//span[text()='Pet Types']")).click();
 
-        driver.findElement(By.xpath("//h2[text()='Not Found - 404 error']"));
+       // System.out.print(
+                driver.findElement(By.xpath("//h1[text()='List of pets']")).getText();
+    }
+    @Test (expected = ClassCastException.class)
+    public void AddOwnerAndMakeTheCheck() { //throws IOException, URISyntaxException{
+        System.setProperty("webdriver.chrome.driver", new File("src/main/resources/chromedriver.exe").getAbsolutePath());
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://localhost:4200/petclinic/");
+
+        driver.findElement(By.xpath("//div[1]/nav/div/ul/li[2]/a")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//li[2]/ul/li[2]/a")).click();
+       // driver.findElement(By.xpath("//li[2]/ul/li[2]/a")).click();;
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id='firstName']")).sendKeys("Korwin");
+        driver.findElement(By.xpath("//*[@id='lastName']")).sendKeys("Oberon");
+        driver.findElement(By.xpath("//*[@id='address']")).sendKeys("BakerStreet 221B");
+        String s="London"+System.currentTimeMillis();
+        driver.findElement(By.xpath("//*[@id='city']")).sendKeys(s);
+        driver.findElement(By.xpath("//*[@id='telephone']")).sendKeys("799999123");
+        driver.findElement(By.xpath("//div[7]/div/button[2]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        String G="coll";
+        //driver.findElement(By.xpath("//span[text()=London]")).click();
+
+           for (int i = 1; ; i++) {//System.out.print(driver.findElement(By.xpath("//div/div/div/table/tbody/tr[" + Integer.toString(i) + "]/td[2]")).getText());
+               //System.out.print(driver.findElement(By.xpath("//div/div/div/table/tbody/tr[" + Integer.toString(i) + "]/td[3]")).getText());
+             //  System.out.print(driver.findElement(By.xpath("//div/div/div/table/tbody/tr[" + Integer.toString(i) + "]/td[4]")).getText()); System.out.println();
+                           try {G = driver.findElement(By.xpath("//tbody/tr[" + Integer.toString(i) + "]/td[1]/a")).getText();} catch (NoSuchElementException E){throw new NullPointerException("The owner was not found"); }
+               if (G.compareTo("Korwin Oberon")==0) System.out.println(G);
+               if (G.compareTo("Korwin Oberon")==0)
+                   if (driver.findElement(By.xpath("//tbody/tr[" + Integer.toString(i) + "]/td[2]")).getText().compareTo("BakerStreet 221B")==0)
+                       if (driver.findElement(By.xpath("//tbody/tr[" + Integer.toString(i) + "]/td[3]")).getText().compareTo(s)==0)
+                           if (driver.findElement(By.xpath("//tbody/tr[" + Integer.toString(i) + "]/td[4]")).getText().compareTo("799999123")==0)
+                               try {if (driver.findElement(By.xpath("//table/tbody/tr["+Integer.toString(i)+"]/td[5]/tbody/tr")).getText().compareTo("THIS MUST ALWAYS FAIL")==0) System.out.print("THIS MUST ALWAYS FAIL");} catch (NoSuchElementException E) {throw new ClassCastException("No pets here, be happy");};
+
+
+                  // System.out.println(G);
+           }
+
+        //System.out.println(G);
     }
 
     @Test
